4f88ef3d8d2b5cb30e04fb957ce17af9a478edde
deyems <adeyemisola11@gmail.com>
2020-11-18 21:02:58 +0100

    create table on your database

---
+++ db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    created_at DATETIME NOT NULL
+);
+
+CREATE TABLE posts(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    user_id INT(11) NOT NULL,
+    created_at DATETIME NOT NULL
+);
+
+CREATE TABLE comments(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    post_id INT(11) NOT NULL,
+    comment VARCHAR(255) NOT NULL,
+    user_id INT(11) NOT NULL,
+    comment_at DATETIME NOT NULL
+);
