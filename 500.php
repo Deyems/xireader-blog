bd88ba7a67c456cb07d3d223c74bebe31a0a1ec7
deyems <adeyemisola11@gmail.com>
2020-11-18 21:02:18 +0100

    500 error page

---
+++ 500.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+    <title>XiReader</title>
+</head>
+<body>
+    <section class="container section">
+        <div class="post">
+            <h1 class="post-title">
+                500 internal server error
+            <p class="post-content"><?php echo $errorMessage ?></p>
+        </div>
+        
+    </section>
+</body>
+</html>
